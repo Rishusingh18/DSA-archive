0d28899fe8a8dc8e3407fc1cc82c5f0db11766e9
Rishusingh18 <194272463+Rishusingh18@users.noreply.github.com>
2026-02-21 13:03:53 +0530

    Leetcode 14,longest prefix

---
+++ leetcode streak/leetcode 14 longest prefix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+
+        sort(strs.begin(), strs.end());
+
+        string first = strs[0];
+        string last  = strs[strs.size() - 1];
+
+        int i = 0;
+        while (i < first.size() && i < last.size()) {
+            if (first[i] != last[i]) {
+                break;
+            }
+            i++;
+        }
+
+        return first.substr(0, i);
+    }
+};
